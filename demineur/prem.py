0d321b80a2f45ddc22b64bfce002f9cafd82ed08
GiulioCampina <93941029+GiulioCampina@users.noreply.github.com>
2026-06-06 23:14:18 +0200

    Update prem.py

--- demineur/prem.py
+++ demineur/prem.py
@@ -8,8 +8,8 @@ st.set_page_config(page_title="Démineur", page_icon=None,  initial_sidebar_stat
 st.session_state["usnm"] = "pas_de_nom"
 st.title("Bienvenue sur le Démineur.")
 st.subheader("Sur ce site internet, vous aurez l'opportunité de jouer au démineur et de comparer votre score avec vos amis.", divider=True)
-st.subheader("La règle du jeu est simple : le joueur sélectionne une case sur la grille, et cette case révèle un
-nombre qui indique le nombre de mines se trouvant dans les cases adjacentes. En utilisant ces informations, le joueur doit déduire l'emplacement des mines et les marquer avec des drapeaux pour les éviter.")
+st.subheader(str(" La règle du jeu est simple : le joueur sélectionne une case sur la grille, et cette case révèle un
+nombre qui indique le nombre de mines se trouvant dans les cases adjacentes. En utilisant ces informations, le joueur doit déduire l'emplacement des mines et les marquer avec des drapeaux pour les éviter." ))
 
 st.subheader("Insère ton nom de joueur et clique sur le bouton rouge pour lancer le jeu.")
 nom = st.text_input("Pseudo")
