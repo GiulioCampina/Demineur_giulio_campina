739eb5809da32dd5d34889d6cf4929bab2b098b9
GiulioCampina <93941029+GiulioCampina@users.noreply.github.com>
2026-06-06 17:34:56 +0200

    Update prem.py

--- demineur/prem.py
+++ demineur/prem.py
@@ -6,11 +6,11 @@ import supabase as sb
 
 st.set_page_config(page_title="Démineur", page_icon=None,  initial_sidebar_state="collapsed", menu_items=None)
 st.session_state["usnm"] = "pas_de_nom"
-st.title("Bienvenue sur le Démineur ")
-st.subheader("Sur ce site internet vous aurez l'opportunitée de jouer au démineur et de comparer votre score avec vos amis", divider=True)
+st.title("Bienvenue sur le Démineur.")
+st.subheader("Sur ce site internet, vous aurez l'opportunité de jouer au démineur et de comparer votre score avec vos amis.", divider=True)
 
 
-st.subheader("Insert ton nom de joueur et clique sur le bouton rouge pour lancer le jeu")
+st.subheader("Insère ton nom de joueur et clique sur le bouton rouge pour lancer le jeu.")
 nom = st.text_input("Pseudo")
 st.session_state["usnm"] = nom
 changer_page = st.button("Allons déminer", use_container_width=True, type="primary")
